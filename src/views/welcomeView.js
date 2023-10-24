a463b17de1f1fdbb47a57b828be4274f0b493ada
liz-garcia <lizgarciamarin@gmail.com>
2023-10-24 23:32:52 +0200

    Finished CSS.

--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -4,11 +4,12 @@ import { ENTER_APP_BUTTON_ID } from "../constants.js";
 export const createWelcomeElement = () => {
   const element = document.createElement("div");
   element.id = "welcome";
+  element.classList.add("centered");
 
   element.innerHTML = String.raw`
     <h1 id="welcome-title">Welcome!</h1>
     <p id="welcome-message">Do you like lofi? Listen to some nice tracks with our app!</p>
-    <button id="${ENTER_APP_BUTTON_ID}">Enjoy :)</button>
+    <button id="${ENTER_APP_BUTTON_ID}">Click Here :)</button>
   `;
 
   return element;
